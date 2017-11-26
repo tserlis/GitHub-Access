5a5192592f56aa750fbdf7b6341299e24d054e72
tserlis <erlist@tcd.ie>
2017-11-26 22:26:03 +0000

    Update GithubAccess.java

--- src/GithubAccess.java
+++ src/GithubAccess.java
@@ -1,21 +1,42 @@
-import java.net.*;
+import java.util.Scanner;
 import java.io.*;
+import org.eclipse.egit.github.core.User;
+import org.eclipse.egit.github.core.client.GitHubClient;
+import org.eclipse.egit.github.core.service.UserService;
 
+
+
+/**
+ * 
+ * @author Thomas Erlis
+ * @version 1.0
+ *
+ */
 public class GithubAccess {
 
-	public static void main(String[] args) {
-	}
-	
-	public void URLReader(String url) throws MalformedURLException, IOException {
-		HttpURLConnection gitCon = (HttpURLConnection) new URL("https://api.github.com/user").openConnection();
-		gitCon.addRequestProperty("User", "Chrome");
-		BufferedReader in = new BufferedReader(new InputStreamReader(gitCon.getInputStream()));
-		
-		String inputLine;
-		while((inputLine = in.readLine()) != null) {
-			System.out.print(inputLine);
+	public static void main(String[] args) throws IOException {
+		System.out.println("Enter your username:\n");
+		Scanner input = new Scanner (System.in);
+		String username = input.next();
+		System.out.println("Enter your password:\n");
+		String password = input.next();
+		input.close();
+		User user = FindUser(username, password);
+		System.out.println("Information on this user:\nLogin: "
+							+ user.getLogin() + "\nName: "
+							+ user.getName() + "\nEmail: "
+							+ user.getEmail() + "\nFollowers: "
+							+ user.getFollowers() + "\nFollowing: "
+							+ user.getFollowing() + "\nPublic Repos: "
+							+ user.getPublicRepos() + "\nPrivate Repos: "
+							+ user.getTotalPrivateRepos());
 		}
-		in.close();
+	
+	private static User FindUser(String username, String password) throws IOException {
+		GitHubClient client = new GitHubClient();
+		client.setCredentials(username, password);
+		UserService userService = new UserService(client);
+		return userService.getUser();
 	}
 
 }
